3fb967c23250d0e3c2f9ac8258cc730d1528a4e3
Richard Jiang Shenhao <richardejiang@hotmail.com>
2016-09-16 10:49:19 +0800

    restructured the MyConcolve function for ff as filter function

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -4,37 +4,45 @@ import os
 
 def MyConvolve(image, ff):
 	result = np.zeros(image.shape)
-	if (ff == 1):
-		kernelV = np.array([(-1, 0, 1), (-1, 0, 1), (-1, 0, 1)])
-		kernelH = np.array([(1, 1, 1), (0, 0, 0), (-1, -1, -1)])
-		#maxValue = np.sqrt((255 * 3) ** 2 * 2)
-		# The max value is obtained by 1*255+1*255+1*255 and go through the same process below
-		title = 'Prewitt'
-	else:
-		kernelV = np.array([(-1, 0, 1), (-2, 0, 2), (-1, 0, 1)])
-		kernelH = np.array([(1, 2, 1), (0, 0, 0), (-1, -2, -1)])
-		#maxValue = np.sqrt((255 * 4) ** 2 * 2)
-		title = 'Sobel'
+	ffConvolution = np.flipud(ff)
+	ffConvolution = np.fliplr(ffConvolution)
+
 	rows = len(image)
 	cols = len(image[0])
 
 	for row in range(1, rows-1):
 		for col in range(1, cols-1):
 			currWindow = image[row-1:row+2, col-1:col+2]
-			strength = np.int(np.sum(currWindow * kernelH) ** 2 + np.sum(currWindow * kernelV) ** 2)
+			strength = np.int(np.sum(currWindow * ffConvolution) ** 2)
 			result[row][col] = strength
 
-	result = np.sqrt(result)
-	maxValue = np.amax(result)
-	# another way of doing normalization is to use the max value in the array
-	result *= (255.0 / np.float(maxValue))
-	result = result.astype('uint8')
+	# result = np.sqrt(result)
+	# maxValue = np.amax(result)
+	# result *= (255.0 / np.float(maxValue))
+	# result = result.astype('uint8')
 
 	return result
 
 def edgeDetection(image, title):
-	PrewittResult = MyConvolve(image, 1)
-	SobelResult = MyConvolve(image, 2)
+	PrewittFilterV = np.array([(1, 0, -1), (1, 0, -1), (1, 0, -1)])
+	PrewittFilterH = np.array([(-1, -1, -1), (0, 0, 0), (1, 1, 1)])
+	SobelFilterV = np.array([(1, 0, -1), (2, 0, -2), (1, 0, -1)])
+	SobelFilterH = np.array([(-1, -2, -1), (0, 0, 0), (1, 2, 1)])
+
+	PrewittResult = MyConvolve(image, PrewittFilterH) + MyConvolve(image, PrewittFilterV)
+	PrewittResult = np.sqrt(PrewittResult)
+	maxValue = np.amax(PrewittResult)
+	minValue = np.amin(PrewittResult)
+	PrewittResult = (PrewittResult - minValue) / np.float(maxValue - minValue) * 255.0
+	PrewittResult = PrewittResult.astype('uint8')
+
+	SobelResult = MyConvolve(image, SobelFilterH) + MyConvolve(image, SobelFilterV)
+	SobelResult = np.sqrt(SobelResult)
+	maxValue = np.amax(SobelResult)
+	minValue = np.amin(SobelResult)
+	SobelResult = (SobelResult - minValue) / np.float(maxValue - minValue) * 255.0
+	SobelResult = SobelResult.astype('uint8')
+
 	cv2.imwrite('prewitt_result_' + title + '.jpg', PrewittResult)
 	cv2.imwrite('sobel_result_' + title + '.jpg', SobelResult)
 	return SobelResult
@@ -53,6 +61,7 @@ def edgeThinning(image, title):
 			# 	image[row][col] = 0
 			# if (image[row][col] == np.amax(image[row])) or (image[row][col] == np.amax(image[:, col])):
 			# 	result[row][col] = image[row][col]
+	result = result.astype('uint8')
 
 	cv2.imwrite('thinned_sobel_result_' + title + '.jpg', result)
 
@@ -64,13 +73,8 @@ def edgeThinningTwo(image, title):
 	cols = len(image[0])
 	for row in range(1, rows-1):
 		for col in range(1, cols-1):
-			# currWindow = image[row-1:row+2, col-1:col+2]
-			# if (currWindow[1][1] == np.amax(currWindow[1])) or (currWindow[1][1] == np.amax(currWindow[:, 1])):
-			# 	pass
-			# else:
-			# 	image[row][col] = 0
 			if (
-				(image[row][col] < image[row-1][col] or image[row][col] < image[row+1][col]) or
+				(image[row][col] < image[row-1][col] or image[row][col] < image[row+1][col]) and
 				(image[row][col] < image[row][col-1] or image[row][col] < image[row][col+1])
 				):
 				image[row][col] = 0
@@ -91,5 +95,5 @@ if (__name__=="__main__"):
 	title = 'example'
 	image = cv2.imread('example.jpg', 0)
 	sobelResult = edgeDetection(image, title)
-	edgeThinningTwo(sobelResult, title)
+	edgeThinning(sobelResult, title)
 	pass
