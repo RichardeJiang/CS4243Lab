fa67d810b3381d5e3c60d020897186d868157166
Richard Jiang Shenhao <richardejiang@hotmail.com>
2016-09-16 11:20:59 +0800

    modified gitignore file to ignore lab submission files

--- Lab4/lab4.py
+++ Lab4/lab4.py
@@ -1,3 +1,15 @@
 import cv2
 import numpy as np
 
+def gauss_kernels(size, sigma=1.0):
+	## return a 2d gaussian gauss_kernels
+	if size<3:
+		size = 3
+	m = size/2
+	x, y=np.mgrid[-m:m+1, -m:m+1]
+	kernel = np.exp(-(x*x + y*y)/(2*sigma*sigma))
+	kernel_sum = kernel.sum()
+	if not sum == 0:
+		kernel = kernel / kernel_sum
+
+	return kernel
